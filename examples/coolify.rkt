9ec415007000ca8649321b2f2162bf71a766abe8
agj <ale@agj.cl>
2015-12-20 13:53:49 +0900

    Adjusted some spacing.

--- examples/coolify.rkt
+++ examples/coolify.rkt
@@ -16,9 +16,9 @@
 boring-square
 
 (hor-align -75
-            (coolify 1 boring-square)
-            (coolify 2 boring-square)
-            (coolify 3 boring-square)
-            (coolify 4 boring-square)
-            (coolify 5 boring-square))
+           (coolify 1 boring-square)
+           (coolify 2 boring-square)
+           (coolify 3 boring-square)
+           (coolify 4 boring-square)
+           (coolify 5 boring-square))
 
