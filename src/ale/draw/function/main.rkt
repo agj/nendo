e70d3a780dad6ad29685c320d5da361ec3aafdf6
agj <ale@agj.cl>
2016-01-13 02:22:32 +0900

    Colorizable draw-function picts.

--- src/ale/draw/function/main.rkt
+++ src/ale/draw/function/main.rkt
@@ -13,17 +13,18 @@
   (dc (λ (dc dx dy)
         (define bm (make-bitmap area-width area-height))
         (define bm-dc (send bm make-dc))
+        (define color (send (send dc get-pen) get-color))
         (for* ([x (in-range (rect-left area) (rect-right area))]
                [y (in-range (rect-bottom area) (rect-top area))])
               (send bm-dc set-pixel
                     (- x (rect-left area))
                     (- y (rect-bottom area))
-                    (to-color (f x y))))
+                    (to-color (f x y) color)))
         (send dc draw-bitmap bm dx dy))
       area-width area-height))
 
-(define (to-color value)
+(define (to-color value color)
   (cond [(is-a? value color%)
          value]
         [(boolean? value)
-         (if value (make-color 0 0 0) (make-color 255 255 255))]))
+         (if value color (make-color 0 0 0 0))]))
--- tests/01.rkt
+++ tests/01.rkt
@@ -1,7 +1,11 @@
 #lang racket
 (require "../src/ale/main.rkt")
 
-(define (reg x y) (< (abs x) 10))
+(define (vertical x y) (< (abs x) 10))
+(define (horizontal x y) (< (abs y) 10))
 
-(colorize "red" (draw-function reg (rect 100 100 -100 -100)))
+
+(superimpose
+ (draw-function horizontal (rect 100 100 -100 -100))
+ (colorize "red" (draw-function vertical (rect 100 100 -100 -100))))
 
